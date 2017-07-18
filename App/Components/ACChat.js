a987be99ce9674c781877cab2fb46d296cbf78e4
Christopher David <chris@arcade.city>
2017-07-18 17:31:00 -0500

    Test send message to chat room

--- App/Components/ACChat.js
+++ App/Components/ACChat.js
@@ -4,6 +4,24 @@ import firebase from '../Config/FirebaseConfig'
 
 export default class ACMap extends Component {
   componentDidMount () {
+    console.tron.log('Lets send a message')
+
+    const db = firebase.database()
+    const roomKey = '-KpMSK6RN0G33Tf5JDae'
+    const now = firebase.database.ServerValue.TIMESTAMP
+
+    db.ref(`messages/${roomKey}`).push({
+      text: 'Now here is a message yoyoyogasm.',
+      user: {
+        _id: 'Fmu6D27WD8ZYecsxt2cu6KuvPH93',
+        name: 'Christopher David',
+        avatar: 'https://scontent.xx.fbcdn.net/v/t1.0-1/p100x100/19149077_872744746215821_3570726456667346281_n.jpg?oh=dc6afd902fca70438d479b66d1e6e917&oe=5A020094'
+      },
+      createdAt: now
+    })
+  }
+
+  registerRoom () {
     console.tron.log('Lets play with chat.')
     const db = firebase.database()
     const roomKey = db.ref(`rooms`).push().key
