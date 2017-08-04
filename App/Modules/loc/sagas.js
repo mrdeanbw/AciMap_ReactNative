ba65dd54c609fdf2050de0e11d0846ccc96ae8cf
Christopher David <chris@arcade.city>
2017-08-04 15:41:08 -0500

    Fix Android location permission issue

--- App/Modules/loc/sagas.js
+++ App/Modules/loc/sagas.js
@@ -1,16 +1,46 @@
 // LOC SAGAS
+import { Platform, PermissionsAndroid } from 'react-native'
 import { store } from '../../Setup/App'
 import LocActions from './redux'
 import { Metrics } from '../../Theme/'
+
 
 /*
 fetchUserLoc
 Called from anywhere. Grab the user loc (what re permissions?) and call updateUserLoc action.
 */
 export function * fetchUserLoc () {
+  if (Platform.OS === 'android') {
+    grabPermissionsAndroid()
+  } else {
+    actuallyFetchUserLoc()
+  }
+}
+
+async function grabPermissionsAndroid () {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        'title': 'Location needed',
+        'message': 'Arcade City needs your location to show drivers near you'
+      }
+    )
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      actuallyFetchUserLoc()
+    } else {
+      window.alert("Location required to use app")
+    }
+  } catch (err) {
+    console.warn(err)
+  }
+}
+
+function actuallyFetchUserLoc () {
   const ASPECT_RATIO = Metrics.screenWidth / Metrics.screenHeight
   const LATITUDE_DELTA = 0.0922
   const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
+
   navigator.geolocation.getCurrentPosition(
     (position) => {
       var loc = {
@@ -38,6 +68,7 @@ export function * fetchUserLoc () {
   })
 
 }
+
 
 export function * fetchUserLocError ({ error }) {
   window.alert('Location needed to use app. Error: "' + error + '"')
